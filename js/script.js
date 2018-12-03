e943cc0a9d29f34f42ccb9fca934d358e5783c09
yevheniia <evgesha.drozdova@gmail.com>
2018-12-03 03:18:25 +0200

    init

--- js/script.js
+++ js/script.js
@@ -3,16 +3,14 @@
  */
 
 
-// var viewBox = $("#chartContainer")[0].getAttribute("viewBox").split(" "),
-//     size = viewBox.slice(2),
     var w = window.innerWidth * 0.9,
         h = window.innerHeight * 0.9,
-        m = {top: 20, left: 20, bottom: 20, right: 20 },
+        m = {top: 10, left: 20, bottom: 20, right: 20 },
         aspect = w / h;
 
 
 var chartWidth  = 250,
-    chartHeight  =  250,
+    chartHeight  =  300,
     barHeight  = 10,
     gapBetweenGroups = 20,
     spaceForLabels   = 150,
@@ -20,8 +18,16 @@ var chartWidth  = 250,
 
 var color = d3.scale.category20();
 
+// var color = d3.scale.ordinal()
+// //this assumes you have 3 groups of data//﻿each of the domains corresponds to a color set
+//     .domain(["SCO reponse", "State reponse", "Russian influence"])
+//     .range(["#b7ffa9", "#c2dbff", "red"]);
+
+
+
+
 var x = d3.scale.linear()
-    .domain([0, 5])
+    .domain([0, 6])
     .range([0, chartWidth]);
 
 var y = d3.scale.linear()
@@ -33,6 +39,12 @@ var yAxis = d3.svg.axis()
     .tickSize(0)
     .orient("left");
 
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .ticks(5)
+    .tickFormat('')
+    .tickSize(-180)
+    .orient("bottom");
 
 
 d3.csv("data/data_eng.csv", function(error, data){
@@ -62,27 +74,37 @@ d3.csv("data/data_eng.csv", function(error, data){
         .enter()
         .append("div")
         .style("width", chartWidth - m.left - m.right + "px")
-        .attr("height", 0)
-        .attr("class", "svgContainer");
+        .attr("height", "300px")
+        .attr("class", "svgContainer")
+
+        ;
 
     div.append("h2")
         .attr("class", "title")
-            .style("background", "red")
+            // .style("background", "red")
             .style("height", "50px")
             .html(function(d) {
                 return d.key
             });
 
-    var svg = div.append("svg")
+    var childDIV =  div.append("div")
+        .attr("class", "childDIV")
+        .style("width", chartWidth - m.left - m.right + "px")
+        .attr("id", function(d, i) {
+            return "c-" + i
+        });
+
+    var svg = childDIV.append("svg")
         .attr("width", chartWidth - m.left - m.right  + "px")
         .attr("class", "shit")
-        .attr("height", 0)
+        .attr("height", 180)
        ;
+
 
 
     var barContainer = svg.append("g")
     .attr("transform", function(d, i) {
-        return "translate(" + 0 +"," + 10 + ")";
+        return "translate(" + 0 +"," + 15 + ")";
     });
 
 
@@ -96,6 +118,8 @@ d3.csv("data/data_eng.csv", function(error, data){
         return "translate(" + 0 + ","  + (i * 60) + ")";
     });
 
+
+
     box.selectAll("rect")
         .data(function(d) {
             return d.values;
@@ -105,24 +129,40 @@ d3.csv("data/data_eng.csv", function(error, data){
         .attr("transform", function(d, i) {
             return "translate(" + 0 + "," + (i * (barHeight + 2)) + ")";
         })
-        .attr("width", function (d){ return x(d.value)})
+        .attr("width", function (d){ console.log(d.value);  return x(d.value)})
         .attr("height", barHeight)
         .attr("fill", function(d){
             return color(d.measure)}
         );
 
     box.append("text")
+        .attr("fill", "white")
+        .attr("y", -2)
         .html(function(d) {
             return d.key
         });
 
 
+    // box.append("g")
+    //     .attr("class", "x axis")
+    //     // .attr("transform", "translate(0, 180)")
+    //     .attr("stroke", "white")
+    //     .attr("opacity", 1)
+    //     .call(xAxis);
+
     $(".title").on("click", function(d) {
 
-      svg
-            // .transition()
-            // .duration(2000)
-            .attr("height", 200 + "px");
+        var t = d3.transition()
+            .duration(500);
+
+        var targetID = $(this)
+            .parent()
+            .find(".childDIV")
+            .attr("id");
+
+         d3.select("#"+targetID)
+            .transition(t)
+            .style("height", "260px");
 
     });
 
@@ -132,12 +172,7 @@ d3.csv("data/data_eng.csv", function(error, data){
 
 });
 
-var t = d3.transition()
-    .duration(500);
 
-d3.select("body")
-    .transition(t)
-    .style("background-color", "black");
 
 
 
